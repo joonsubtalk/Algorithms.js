60030574a44e391aaae2d3c553c4c1519e22c488
Unknown <alvinhochoi@gmail.com>
2018-03-21 17:40:02 -0700

    Complete Part B

--- Question1.js
+++ Question1.js
@@ -12,5 +12,23 @@ function findDuplicate(arr){
 	return dupArr;
 }
 
+// Part B
 
-// Part B 
+function removeDuplicates(arr){
+	 
+	for (var i = 0; i < arr.length; i++){
+		
+		var dup = 0;
+
+		while (dup >= 0){
+			dup = arr.slice(i+1).indexOf(arr[i]);
+			if (dup > -1){
+				var arr1 = arr.slice(0, dup+1+i);
+				var arr2 = arr.slice(dup+2+i);
+				arr = arr1.concat(arr2);
+			}							
+		}
+	}
+
+	return arr;
+}
